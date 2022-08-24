adf34d5ee9539c35b3c0322a625637903ec52c56
Ivan <aguas.xavier2013a@gmail.com>
2022-08-24 16:16:35 -0500

    add entity

--- backend/app/api/api_v1/endpoints/entity.py
+++ backend/app/api/api_v1/endpoints/entity.py
@@ -1,7 +1,7 @@
 
 from ast import alias
 from pydoc import text
-from google.cloud.dialogflow_v2 import EntityTypesAsyncClient, ListEntityTypesRequest, AgentsAsyncClient
+from google.cloud.dialogflow_v2 import EntityTypesAsyncClient, ListEntityTypesRequest, AgentsAsyncClient, EntityType, CreateEntityTypeRequest, GetEntityTypeRequest
 from pprint import pprint
 from fastapi import Body, APIRouter, HTTPException, Query
 from typing import Any, List, Sequence
@@ -30,14 +30,63 @@ async def get_intent_list(
     # Make the request
     entity_agent_client = EntityTypesAsyncClient()
     response = await entity_agent_client.list_entity_types(request=request)
-    print('response', response._response)
+    entities = response._response.entity_types
 
-    # intent_output = []
-    # for intent in intents:
-    #     new_intent = MessageToDict(intent._pb)
-    #     intent_output.append(new_intent)
+    entity_output = []
+    for entity in entities:
+        new_entity = MessageToDict(entity._pb)
+        entity_output.append(new_entity)
+        print('new_entity', new_entity)
         
 
-    # return IGetResponseBase(data=intent_output)
+    return IGetResponseBase(data=entity_output)
 
 
+@router.get("/entity/{project_id}", response_model=IGetResponseBase)
+async def get_entity_type(
+    project_id: str = "mybotivantest",
+    entity_id:  str = Query(default="cf62c5d8-2099-46aa-a0a0-9ccdec85ab81", description="ID Intent"),
+) -> Any:
+
+    # Create a client
+    client = EntityTypesAsyncClient()
+
+    # Initialize request argument(s)
+    request = GetEntityTypeRequest(
+        name="name_value",
+    )
+
+    # Make the request
+    response = await client.get_entity_type(request=request)
+
+    # Handle the response
+    print(response)
+
+
+
+@router.post("/entity/create/{project_id}", response_model=IPostResponseBase)
+async def create_entity_type(
+    project_id: str = "mybotivantest",
+    display_name: str = Body(default="color")
+) -> Any:
+
+     # Create a client
+    agent_client = AgentsAsyncClient()
+    parent = agent_client.agent_path(project_id)
+
+    # Initialize request argument(s)
+    entity_type = EntityType()
+    entity_type.display_name = display_name
+    entity_type.kind = "KIND_REGEXP"
+
+    request = CreateEntityTypeRequest(
+        parent=parent,
+        entity_type=entity_type,
+    )
+
+    # Make the request
+    entity_agent_client = EntityTypesAsyncClient()
+    response = await entity_agent_client.create_entity_type(request=request)
+
+    # Handle the response
+    print(response)
